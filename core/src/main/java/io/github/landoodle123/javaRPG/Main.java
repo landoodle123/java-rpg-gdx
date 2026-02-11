2fb3c754c8daa9e1d093c511796d5fd91b7dc282
Landon <landonkazen@gmail.com>
2026-02-10 19:22:52 -0500

    added little guy

--- core/src/main/java/io/github/landoodle123/javaRPG/Main.java
+++ core/src/main/java/io/github/landoodle123/javaRPG/Main.java
@@ -7,25 +7,25 @@ import com.badlogic.gdx.utils.ScreenUtils;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture npc;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        npc = new Texture("guy.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(npc, 140, 210);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        npc.dispose();
     }
 }
