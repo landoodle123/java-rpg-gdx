62595f7d1a11974503142fa0caa917ad1a3279da
Landon <landonkazen@gmail.com>
2026-03-02 09:24:27 -0500

    fixed multithreading and dialog

--- core/src/main/java/io/github/landoodle123/javaRPG/Main.java
+++ core/src/main/java/io/github/landoodle123/javaRPG/Main.java
@@ -4,14 +4,17 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import javax.swing.*;
 
+import javax.swing.*;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static com.badlogic.gdx.Gdx.graphics;
@@ -26,8 +29,35 @@ public class Main extends ApplicationAdapter {
     private Texture npcTexture;
     private FitViewport viewport;
     static JFrame f;
+    public static Boolean stopt1 = false;
+    static ExecutorService executor = Executors.newFixedThreadPool(3);
+    public static Runnable runTalk = new Runnable() {
+
+        @Override
+        public void run() {
+            try {
+                while (!stopt1) {
+                    talk();
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Failed with exception: " + e);
+            }
+        }
+    };
     //ShapeRenderer npcShape = new ShapeRenderer();
     //ShapeRenderer playerShape = new ShapeRenderer();
+    /**static Thread t1 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while (!stopt1) {
+                try {
+                    talk();
+                } catch (Exception e) {
+                    System.out.println("Talk failed with exception " + e);
+                }
+            }
+        }
+    });**/
 
     @Override
     public void create() {
@@ -57,54 +87,70 @@ public class Main extends ApplicationAdapter {
     }
     public static void use() {
         //TODO: Add logic for use, either picking up an item or interacting with an npc or door
-        if (playerRectangle.overlaps(npcRectangle)) {
-            talk();
+        /**if (playerRectangle.overlaps(npcRectangle) && t1.getState() == Thread.State.NEW) {
+            t1.start();
+        } else if (playerRectangle.overlaps(npcRectangle) && t1.getState() == Thread.State.TERMINATED) {
+            System.out.println("Thread was terminated");
+            try {t1.start();} catch (Exception e) {
+                System.out.println("Thread failed to start with exception: " + e);
+            }
         } else {
-            System.out.println("Neither overlapping npc or door");
+            if (t1.getState() != Thread.State.NEW) {
+                System.out.println("thread already running or in broken state");
+                System.out.println(t1.getState() + " = t1 state");
+            } else {
+                System.out.println("Player not overlapping anything.");
+                System.out.println(t1.getState() + " = t1 state");
+            }
+        }**/
+        try {executor.submit(runTalk);} catch(Exception e) {
+            System.out.println("Failed with exception: " + e);
         }
     }
     private void input() {
         float speed = 1f;
         float delta = graphics.getDeltaTime();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && playerCharacter.getX() < 7) {
             playerCharacter.setX(playerCharacter.getX() + speed * delta);
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && playerCharacter.getX() > 0) {
             playerCharacter.setX(playerCharacter.getX() - speed * delta);
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && playerCharacter.getY() < 7) {
             playerCharacter.setY(playerCharacter.getY() + speed * delta);
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && playerCharacter.getY() > 0) {
             playerCharacter.setY(playerCharacter.getY() - speed * delta);
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
             System.out.println("space pressed");
+            stopt1 = false;
             use();
         }
     }
-    public static void talk()
-    {
-        JDialog d = new JDialog(f, "Conversation");
-        int dialogueSelection = ThreadLocalRandom.current().nextInt(0, 3);
+    public static void talk() throws InterruptedException {
+            JDialog d = new JDialog(f, "Conversation");
+            int dialogueSelection = ThreadLocalRandom.current().nextInt(0, 3);
 
-        // create a label
-        JLabel l = new JLabel(String.format("<html><body style='width: 350px; align: center'<p>%s</p></html>", dialogueOptions[dialogueSelection]));
+            // create a label
+            JLabel l = new JLabel(String.format("<html><body style='width: 350px; align: center'<p>%s</p></html>", dialogueOptions[dialogueSelection]));
 
-        d.add(l);
+            d.add(l);
 
-        // setsize of dialog
-        d.setSize(400, 400);
+            // setsize of dialog
+            d.setSize(400, 400);
 
-        d.setLocation(400, 400);
+            d.setLocation(400, 400);
 
-        // set visibility of dialog
-        d.pack();
-        d.setVisible(true);
-        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            // set visibility of dialog
+            d.pack();
+            d.setVisible(true);
+            TimeUnit.SECONDS.sleep(15);
             d.setVisible(false);
-        }
+            stopt1 = true;
+            d.dispose();
+            System.out.println("If this message is showing something has probably gone wrong.");
     }
     private void logic() {
         float worldWidth = viewport.getWorldWidth();
@@ -113,6 +159,7 @@ public class Main extends ApplicationAdapter {
         float playerWidth = playerCharacter.getWidth();
         float npcWidth = npc.getWidth();
         float npcHeight = npc.getWidth();
+
         //playerRectangle.setX(MathUtils.clamp(playerCharacter.getX(), 0, worldWidth - playerWidth));
         //playerRectangle.setY(MathUtils.clamp(playerCharacter.getY(), 0, worldHeight - playerHeight));
         //npcRectangle.setX(MathUtils.clamp(npc.getX(), 0, worldWidth - npcWidth));
@@ -150,6 +197,6 @@ public class Main extends ApplicationAdapter {
        spriteBatch.dispose();
        npcTexture.dispose();
        charTexture.dispose();
-
+       executor.shutdown();
     }
 }
